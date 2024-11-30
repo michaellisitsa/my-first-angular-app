7aa81fddcb5fd4cb59173268b5c01c50e514bb52
Michael Lisitsa <michael.lisitsa@gmail.com>
2024-11-30 13:41:14 +1100

    Use ngFor to render array of child components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { Housinglocation } from '../housinglocation';
+import HOUSING_LOCATION_LIST from '../constants/HOUSING_LOCATION_LIST';
 
 @Component({
   selector: 'app-home',
@@ -15,6 +16,7 @@ import { Housinglocation } from '../housinglocation';
     </section>
     <section class="results">
       <app-housing-location
+        *ngFor="let housingLocation of housingLocationList"
         [housingLocation]="housingLocation"
       ></app-housing-location>
     </section>
@@ -22,17 +24,5 @@ import { Housinglocation } from '../housinglocation';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
-
-  // Passing props to component
-  housingLocation: Housinglocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
+  housingLocationList: Housinglocation[] = HOUSING_LOCATION_LIST;
 }
