5786445e6b5cf1e6f02a0f8256751a7ba856d027
Michael Lisitsa <michael.lisitsa@gmail.com>
2024-11-30 12:46:45 +1100

    Add home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
+import { HomeComponent } from './home/home.component';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [HomeComponent],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-first-angular-app';
