c42742c5610d2f13c4512995f1fc851badfa3542
Michael Lisitsa <michael.lisitsa@gmail.com>
2024-11-30 13:19:09 +1100

    Create housing location component with inputs

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [],
+  template: `<h1>Housing Locations</h1>`,
+  styleUrl: './housing-location.component.css',
+})
+export class HousingLocationComponent {}
