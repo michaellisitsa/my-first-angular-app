2712146effcddc452ca379d78b35ba4a5c9747c2
Michael Lisitsa <michael.lisitsa@gmail.com>
2024-12-08 15:09:46 +1100

    Add router query inside detail component

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-details',
   imports: [],
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+  styleUrl: './details.component.css',
 })
 export class DetailsComponent {
-
+  housingLocationId = -1;
+  constructor(private route: ActivatedRoute) {
+    this.housingLocationId = Number(route.snapshot.params['id']);
+  }
 }
