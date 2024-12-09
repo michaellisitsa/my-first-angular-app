1065263748214993f293fcca48a3b9de7d37c347
Michael Lisitsa <michael.lisitsa@gmail.com>
2024-12-09 20:06:06 +1100

    Add form to page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 import { Housinglocation } from '../housinglocation';
 import { HousingService } from '../housing.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css',
 })
 export class DetailsComponent {
   housingLocation: Housinglocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
 
   constructor(
     private route: ActivatedRoute,
@@ -19,5 +27,13 @@ export class DetailsComponent {
     const housingLocationId = Number(route.snapshot.params['id']);
     this.housingLocation =
       this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
   }
 }
